ad7ced060a59624e1dcd9b5cd2d1fe43e3a7c56a
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2014-01-21 17:24:06 +0900

    frame test

--- t/test_eval.scm
+++ t/test_eval.scm
@@ -23,6 +23,54 @@
 (test* "'(hoge) is tagged hoge" #f (tagged-list? (read-from-string "'(hoge)") 'hoge))
 (test* "'(hoge) is tagged quoted" #t (tagged-list? (read-from-string "'(hoge)") 'quote))
 
-(test-section "assignment")
+(test-section "frame")
+
+(define f (make-frame '(a) '(1)))
+
+(test* "vars"
+       '(a)
+       (frame-variables f))
+
+(test* "vals"
+       '(1)
+       (frame-values f))
+
+(add-binding-to-frame! 'b 2 f)
+
+(test* "add-binding-to-frame! (vars)"
+       '(b a)
+       (frame-variables f))
+
+(test* "add-binding-to-frame! (vals)"
+       '(2 1)
+       (frame-values f))
+
+(test-section "env")
+
+(define e1 (extend-envrironment '(a b) '(1 2) the-empty-environment))
+
+(test* "first-frame (vars)"
+       '(a b)
+       (frame-variables (first-frame e1)))
+
+(test* "first-frame (vals)"
+       '(1 2)
+       (frame-values (first-frame e1)))
+
+(test* "lookup a" 1 (lookup-variable-value 'a e1))
+(test* "lookup b" 2 (lookup-variable-value 'b e1))
+
+(add-binding-to-frame! 'b 3 (first-frame e1))
+
+(test* "add-binding-to-frame! b -> 3 (vars)"
+       '(b a b)
+       (frame-variables (first-frame e1)))
+
+(test* "add-binding-to-frame! b -> 3 (vals)"
+       '(3 1 2)
+       (frame-values (first-frame e1)))
+
+(test* "lookup a" 1 (lookup-variable-value 'a e1))
+(test* "lookup b" 3 (lookup-variable-value 'b e1))
 
 (test-end)
